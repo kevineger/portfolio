4579cbdb3ee2b5359719db5fa67ddb9abde7a55c
kevineger <egerkevinjames@gmail.com>
2015-11-28 22:31:57 -0800

    Styles new navbar

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 	      <link href='http://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
 	  </head>
 	  <body>
-	  	<a href="#aboutMe">
+	  	<a href="#about">
 	  		<div class="logo-container">
 	  			<img src="images/ke_logo.png" class="logo">
 	  		</div>
@@ -36,35 +36,10 @@
 	  		<div class="contact-container">
 	  			<a href="mailto:egerkevinjames@gmail.com"><p>Contact Me</p></a>
 	  		</div>
-	  	</a>
-
-	  	<!-- <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-	  		<div class="container-fluid">
-	  			Brand and toggle get grouped for better mobile display
-	  			<div class="navbar-header">
-	  				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-	  					<span class="sr-only">Toggle navigation</span>
-	  					<span class="icon-bar"></span>
-	  					<span class="icon-bar"></span>
-	  					<span class="icon-bar"></span>
-	  				</button>
-	  			</div>
-
-	  			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	  				<ul class="nav navbar-nav">
-	  					<li><a href="#aboutMe" class="active">About Me</a></li>
-	  					<li><a href="#education">Education</a></li>
-	  					<li><a href="#projects">Projects</a></li>
-	  					<li><a href="#skills">My Skills</a> </li>
-	  					<li><a href="#hobbies">Hobbies</a></li>
-	  				</ul>
-	  			</div>
-	  		</div>
-	  	</nav> -->
-
+	  	</a>=
 	  	<nav id="topnav" class="navbar-fixed-top">
 	  		<ul>
-	  			<li><a href="#aboutMe" class="active">About</a></li>
+	  			<li><a href="#about" class="active">About</a></li>
 	  			<li><a href="#education">Education</a></li>
 	  			<li><a href="#projects">Projects</a></li>
 	  			<li><a href="#skills">Skills</a> </li>
@@ -90,6 +65,7 @@
 	  	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 
 	  	<script type="text/javascript">
+	  		// Corret the active states of the navbar on scroll and click
 	  		var sections = $('.wrapper')
 	  		, nav = $('nav')
 	  		, nav_height = nav.outerHeight();
@@ -107,12 +83,31 @@
 
 	  					$(this).addClass('active');
 	  					nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+
+	  					// Toggle the box separator colours depending on page
+	  					switch($(this).attr('id')){
+	  						case 'about':
+	  							nav.find('a').removeClass('education projects skills hobbies').addClass('about');
+	  							break;
+	  						case 'education':
+	  							nav.find('a').removeClass('about projects skills hobbies').addClass('education');
+	  							break;
+	  						case 'projects':
+	  							nav.find('a').removeClass('about education skills hobbies').addClass('projects');
+	  							break;
+	  						case 'skills':
+	  							nav.find('a').removeClass('about education projects hobbies').addClass('skills');
+	  							break;
+	  						case 'hobbies':
+	  							nav.find('a').removeClass('about education projects skills').addClass('hobbies');
+	  							break;
+	  					}
 	  				}
 	  			});
 	  		});
+	  		// Smooth scroll between sections
 	  		nav.find('a').on('click', function () {
 	  			var $el = $(this)
-	  			console.log($el)
 	  			, id = $el.attr('href');
 
 	  			$('html, body').animate({
@@ -121,6 +116,8 @@
 
 	  			return false;
 	  		});
+	  		// Toggle colours of separating blocks in navbar
+
 	  	</script>
 	  </body>
 	  </html>
